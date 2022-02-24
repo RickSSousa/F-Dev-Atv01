6a1dc16c7688bbaa541f32377f646cb7b8b41c7e
Ricardo Santos <ricksantos.rs25@gmail.com>
2022-02-24 00:44:33 -0300

    Model, View e Controller Convidados

---
+++ Ex01/Controllers/ConvidadosController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ex01.Models;
+
+namespace Ex01.Controllers
+{
+    public class ConvidadosController : Controller
+    {
+        private static IList<Convidado> convidados = new List<Convidado>()
+        {
+            new Convidado()
+            {
+                ConvidadoId = 1,
+                Nome = "Ricardo S.S.",
+                Email = "rick@gmail.com",
+                Telefone = "932927266",
+                Comparecimento = null
+            },
+            new Convidado()
+            {
+                ConvidadoId = 2,
+                Nome = "Gustavo Guanabara",
+                Email = "gustavog@gmail.com",
+                Telefone = "984752657",
+                Comparecimento = null
+            },
+            new Convidado()
+            {
+                ConvidadoId = 3,
+                Nome = "Nélio Alves",
+                Email = "nelio@gmail.com",
+                Telefone = "932732466",
+                Comparecimento = null
+            },
+            new Convidado()
+            {
+                ConvidadoId = 4,
+                Nome = "Lance Vance",
+                Email = "lance@gmail.com",
+                Telefone = "955246875",
+                Comparecimento = null
+            },
+        };
+        public IActionResult Index()
+        {
+            return View(convidados);
+        }
+    }
+}
---
+++ Ex01/Models/Convidado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex01.Models
+{
+    public class Convidado
+    {
+        public int ConvidadoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public bool? Comparecimento { get; set; }
+    }
+}
